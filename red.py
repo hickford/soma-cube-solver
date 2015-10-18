013fd50ecf764e4ad75fbb8002c32bd6dea75bac
unknown <mirth.hickford@gmail.com>
2015-10-18 23:00:35 +0100

    12 rotations

--- red.py
+++ red.py
@@ -1,14 +1,17 @@
 # pieces by height
 raw = """
-1   2  211 2   2
-11  21 1   11  1
-111  1 1       11""".lstrip()
+1   2   211 2   2  
+11  21  1   11  1  
+111  1  1       11 """.lstrip()
+
+raw_pieces = ["\n".join(line[4*i:4*i+3] for line in raw.splitlines()) for i in range(5)]
 
 import numpy
 
 def from_heights(s):
+    """Construct 3d array from 2d string of heights"""
     lines = s.splitlines()
-    coords = list()
+    shape = numpy.zeros((3,3,3), int)
     for y, line in enumerate(lines):
         for x, h in enumerate(line):
             try:
@@ -16,11 +19,47 @@ def from_heights(s):
             except ValueError:
                 continue
             for z in range(h):
-                coords.append(numpy.matrix([[x],[y],[z]]))
-    return numpy.hstack(coords)
+                shape[x,y,z] = 1
+    return shape
 
-assert numpy.array_equal(from_heights("31\n2"), numpy.matrix(
-    [[0, 0, 0, 1, 0, 0],
-     [0, 0, 0, 0, 1, 1],
-     [0, 1, 2, 0, 0, 1]]))
+pieces = [from_heights(s) for s in raw_pieces]
 
+def rotations(polycube):
+    for i in range(3):
+        # permute axes xyz, yzx, zxy
+        polycube = numpy.transpose(polycube, (1, 2, 0))
+        for turns in range(4):
+            polycube = numpy.rot90(polycube)
+            yield polycube
+
+def more(polycube):
+    # axes -xzy
+    polycube = numpy.flipud(polycube)
+    polycube = numpy.swapaxes(polycube, 1, 2)
+
+def rot90(m, k=1, axis=2):
+    """Rotate an array by 90 degrees in the counter-clockwise direction around the given axis"""
+    return numpy.swapaxis(numpy.rot90(numpy.swapaxes(m, 2, axis), k), 2, axis)
+
+def solve(pieces):
+    # loop over possibles
+    pass
+
+if __name__ == "__main__":
+    two = numpy.array([[[1, 0],
+        [1, 0]],
+
+       [[1, 1],
+        [0, 0]]])
+
+    three = numpy.array([[[1, 1, 0],
+        [1, 1, 0],
+        [0, 0, 0]],
+
+       [[0, 0, 0],
+        [1, 0, 0],
+        [1, 0, 0]],
+
+       [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]])
