74c57730b3d1e55187e799f7d0acf2d72bfed694
M Hickford <mirth.hickford@gmail.com>
2023-10-31 20:48:34 +0000

    simplify following https://stackoverflow.com/revisions/33190472/9

--- soma-cube-solver.py
+++ soma-cube-solver.py
@@ -25,34 +25,33 @@ def from_heights(s):
 pieces = [from_heights(s) for s in raw_pieces]
 # to do: sort largest to smallest
 
+from numpy import rot90, array
+
 def rotations24(polycube):
+    """List all 24 rotations of the given 3d array"""
+    def rotations4(polycube, axes):
+        """List the four rotations of the given 3d array in the plane spanned by the given axes."""
+        for i in range(4):
+             yield rot90(polycube, i, axes)
+
     # imagine shape is pointing in axis 0 (up)
 
     # 4 rotations about axis 0
-    yield from rotations4(polycube, 0)
+    yield from rotations4(polycube, (1,2))
 
     # rotate 180 about axis 1, now shape is pointing down in axis 0
     # 4 rotations about axis 0
-    yield from rotations4(rot90(polycube, 2, axis=1), 0)
+    yield from rotations4(rot90(polycube, 2, axes=(0,2)), (1,2))
 
     # rotate 90 or 270 about axis 1, now shape is pointing in axis 2
     # 8 rotations about axis 2
-    yield from rotations4(rot90(polycube, axis=1), 2)
-    yield from rotations4(rot90(polycube, -1, axis=1), 2)
+    yield from rotations4(rot90(polycube, axes=(0,2)), (0,1))
+    yield from rotations4(rot90(polycube, -1, axes=(0,2)), (0,1))
 
     # rotate about axis 2, now shape is pointing in axis 1
     # 8 rotations about axis 1
-    yield from rotations4(rot90(polycube, axis=2), 1)
-    yield from rotations4(rot90(polycube, -1, axis=2), 1)
-
-def rotations4(polycube, axis):
-    """List the four rotations of the given cube about the given axis."""
-    for i in range(4):
-        yield rot90(polycube, i, axis)
-
-def rot90(m, k=1, axis=2):
-    """Rotate an array by 90 degrees in the counter-clockwise direction around the given axis"""
-    return numpy.swapaxes(numpy.rot90(numpy.swapaxes(m, 2, axis), k), 2, axis)
+    yield from rotations4(rot90(polycube, axes=(0,1)), (0,2))
+    yield from rotations4(rot90(polycube, -1, axes=(0,1)), (0,2))
 
 def solve(progress, pieces):
     if not pieces:
